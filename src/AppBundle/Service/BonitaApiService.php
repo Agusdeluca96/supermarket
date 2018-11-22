5b98f6a29882926ec20e26be24c35bae4636c85b
Rodrigo PAIT <rodrigo.pait@MJGM.GBA.GOB.AR>
2018-11-22 15:40:58 -0300

    update bonitaApiService

--- src/AppBundle/Service/BonitaApiService.php
+++ src/AppBundle/Service/BonitaApiService.php
@@ -9,42 +9,80 @@ class BonitaApiService
 
     private $urlBonitaApi;
     private $container;
+    private $caseId;
+    private $token;
+    private $processId;
 
     public function __construct(String $urlBonitaApi, ContainerInterface $container) {
         $this->urlBonitaApi = $urlBonitaApi;
         $this->container = $container;
     }
 
-    public function getCaseId($processId, $variables ) {
+    public function getTask($processId){
+
+    }
+
+    public function setCaseVariable($caseVariable, $variableType, $variableValue){
+        $client = $this->container->get('bonita.client');
+        $response = $this->createClient()->request(
+        'PUT',
+        "API/bpm/caseVariable/$caseId/$caseVariable",
+        [
+          'headers' => [
+            'X-Bonita-API-Token' => $token->getValue()
+          ],
+          'json' => [
+            'type' => $variableType,
+            'value' => $variableValue
+          ]
+        ]
+      );
+      $body = $response->getBody();
+      return json_decode($body);
+    }
+
+    public function getCaseVariable($caseVariable){
         $client = $this->container->get('bonita.client');
         $token = $this->login();
+        $response = $this->createClient()->request(
+        'GET',
+        "API/bpm/caseVariable/$caseId/$caseVariable",
+        [
+          'headers' => [
+            'X-Bonita-API-Token' => $token->getValue()
+          ]
+        ]
+      );
+      $body = $response->getBody();
+      return json_decode($body);
+    }
+
+    public function getCaseId($processId, $variables ) {
+        $client = $this->container->get('bonita.client');
+        $processId = $this->getProcessId();
         // $variables = array(['name' => $nombreVariable, 'value' => $valorVariable]);
-        $json = json_encode(['processDefinitionId' => $idProceso, 'variables' => $variables]);
-        try {
-            $response = $this->createClient()->request(
-                'POST',
-                'API/bpm/case/',
-                [
-                    'headers' => [
-                        'X-Bonita-API-Token' => $token->getValue()
-                    ],
-                    'body' => $json
-                ]
-            );
-            $body = $response->getBody();
-            return json_decode($body)->{'id'};
+        $json = json_encode(['processDefinitionId' => $processId, 'variables' => $variables]);
+        $response = $this->createClient()->request(
+            'POST',
+            'API/bpm/case/',
+            [
+                'headers' => [
+                    'X-Bonita-API-Token' => $token->getValue()
+                ],
+                'body' => $json
+            ]
+        );
+        $body = $response->getBody();
+        $caseId = json_decode($body)->{'id'};
+        return $caseId;
     }
 
     public function getProcessId() {
         $token = $this->login();
-
         $client = $this->container->get('bonita.client');
-        //dump($client);die;
-
-        //$response = $client->get('bonita/API/bpm/process?c=10&p=0');
         $response = $client->request('GET', 'bonita/API/bpm/process?c=10&p=0',[
                 'headers' =>[
-                    'X-Bonita-API-Token' => $token->getValue() //se debe pasar la api de bonita en el header para que tenga efecto el request
+                    'X-Bonita-API-Token' => $token->getValue()
                 ]
             ]
         );
